057cedd04d451ebcde9cb00d9cc12dae5e248056
Artyom Koroteev <artyom.koroteev@gmail.com>
2018-08-11 18:47:54 +0300

    path correction

--- gulp/gulpConfig.js
+++ gulp/gulpConfig.js
@@ -1,9 +1,10 @@
 module.exports = [
 
-  './gulp/tasks/pug.js',
+  './gulp/tasks/pug',
   './gulp/tasks/sass',
   './gulp/tasks/scripts',
   './gulp/tasks/images',
+  './gulp/tasks/clean',
   './gulp/tasks/browserSync',
   './gulp/tasks/watch'
 
